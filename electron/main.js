4d8e803bd434f9958f83b6268b8b01f7e1eccb7c
avelazquezvizcay <avelazquezvizcay@hotmail.com>
2026-04-30 09:23:37 -0400

    chore: background execution and build warning suppression

--- electron/main.js
+++ electron/main.js
@@ -1,9 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 const isDev = !app.isPackaged;
 const { startServer } = require('./server');
 
 let mainWindow;
+let tray = null;
+let isQuitting = false;
 
 function createWindow() {
   const { width, height } = require('electron').screen.getPrimaryDisplay().workAreaSize;
@@ -17,7 +19,7 @@ function createWindow() {
       contextIsolation: true,
       webSecurity: false 
     },
-    icon: path.join(__dirname, '../src/favicon.ico')
+    icon: path.join(__dirname, '../public/sec_icon.png')
   });
 
   const startUrl = isDev 
@@ -30,6 +32,14 @@ function createWindow() {
     mainWindow.show();
   });
 
+  mainWindow.on('close', function (event) {
+    if (!isQuitting) {
+      event.preventDefault();
+      mainWindow.hide();
+      return false;
+    }
+  });
+
   mainWindow.on('closed', function () {
     mainWindow = null;
   });
@@ -39,20 +49,53 @@ function createWindow() {
   }
 }
 
+function createTray() {
+  const iconPath = path.join(__dirname, '../public/sec_icon.png');
+  tray = new Tray(iconPath);
+  
+  const contextMenu = Menu.buildFromTemplate([
+    { 
+      label: 'Abrir SEC', 
+      click: () => {
+        mainWindow.show();
+      } 
+    },
+    { type: 'separator' },
+    { 
+      label: 'Salir', 
+      click: () => {
+        isQuitting = true;
+        app.quit();
+      } 
+    }
+  ]);
+
+  tray.setToolTip('SEC - Estructura de Costos');
+  tray.setContextMenu(contextMenu);
+
+  tray.on('double-click', () => {
+    mainWindow.show();
+  });
+}
+
 app.on('ready', () => {
   // Start the embedded backend server
   startServer();
   createWindow();
+  createTray();
 });
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
-    app.quit();
+    // We don't quit here because we want to stay in background (tray)
+    // app.quit();
   }
 });
 
 app.on('activate', function () {
   if (mainWindow === null) {
     createWindow();
+  } else {
+    mainWindow.show();
   }
 });;
